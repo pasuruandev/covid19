9508fe9e517c3c778bee7332287c3b0d1c6f6285
Ifan Fairuz <ifanfairuz@gmail.com>
2020-04-02 21:24:23 +0700

    add format_date

--- app/helpers.php
+++ app/helpers.php
@@ -15,3 +15,10 @@ function generated_menus() {
 function menu_active() {
     return \Modul\Menus::getActive();
 }
+
+function format_date($date, $formatout = "Y-m-d", $formatin = "d/m/Y") {
+	if (!@$date) return false;
+	
+	$date = \Carbon\Carbon::createFromFormat($formatin, $date);
+	return $date->locale(env('APP_LANG'))->format($formatout);
+}
