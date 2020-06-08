9c627c15b12ad88f9fbc6150a20fbca3e82ca540
imamsutono <imamssutono@gmail.com>
2020-06-08 19:55:42 +0700

    Sync with remote repo

--- resources/views/landing.php
+++ resources/views/landing.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="id">
 <head><meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<title>Data Pantauan COVID-19 Kabupaten Pasuruan</title>
+	<title>Data Pantauan COVID-19 Pasuruan Raya</title>
 	
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<meta name="theme-color" content="#2C9B5B" />
